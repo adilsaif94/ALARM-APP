e29884941eb60ebd363b8c34dbb471897b3bd2dc
adilsaifi002 <adilsaifi002>
2024-07-30 17:36:12 +0530

    changes

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AlarmClock from './Screen/AlarmClock'
+
+const App = () => {
+  return (
+    <>
+      <AlarmClock />
+    </>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
